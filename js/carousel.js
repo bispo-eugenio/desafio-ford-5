867deaa183892496e18039ad38b78373fba0760f
bispo-eugenio <bispoeugenio270@gmail.com>
2025-08-26 16:44:43 -0300

    feat: adiciona funcionalidade no carrosel

--- js/carousel.js
+++ js/carousel.js
@@ -25,7 +25,7 @@ class Carousel {
             if(arr.length > 0){
                 Carousel._size = arr.length;
                 Carousel.Next(arr, Carousel._sequence, Carousel._size); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(arr, Carousel._sequence, Carousel._size); },5000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(arr, Carousel._sequence, Carousel._size); },2000);
             }
             
         } else {
@@ -34,15 +34,30 @@ class Carousel {
     }
 
     static Next(arr, sequece, length){
+
         const elementId = document.getElementById("carousel");
+        const elementIDTitle = document.getElementById("carousel-title")
+
+        const anchor = document.createElement("a");
         const img = document.createElement("img");
-        const imgCollection = document.getElementsByTagName("img")
-        console.log(sequece)
-        console.log(imgCollection.length)
-        if(imgCollection.length >= 1)
+
+        anchor.classList = "carousel-title__link"
+        img.classList = "carousel-title__image"
+
+        const anchorCollection = document.getElementsByClassName("carousel-title__link");
+        const imgCollection = document.getElementsByClassName("carousel-title__image");
+
+        if(imgCollection.length >= 1) 
             imgCollection[0].remove()
-        img.src = arr[sequece].image
+        if(anchorCollection.length > 0)
+            anchorCollection[0].remove()
+
+        img.src = arr[sequece].image;
+        anchor.textContent = arr[sequece].title
+        
         elementId.appendChild(img)
+        elementIDTitle.appendChild(anchor)
+
         Carousel._sequence += 1;
         if (sequece === (length - 1))
             Carousel._sequence = 0
