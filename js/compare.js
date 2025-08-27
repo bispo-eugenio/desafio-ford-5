abe83631d8cd975d04638598278f6a92b2e66bca
bispo-eugenio <bispoeugenio270@gmail.com>
2025-08-27 11:30:17 -0300

    feat: adiciona funcionalidade da pagina lancamento

--- js/compare.js
+++ js/compare.js
@@ -37,16 +37,33 @@ function SetCarToCompare(el, carClass) {
                 el.checked = false
                 return
             }
-            else if (GetCarArrPosition(carArr, carClass) === -1)
-                carArr.push(carClass)        
+            else if (GetCarArrPosition(carArr, carClass) === -1){
+                carArr.push(carClass)
+                verificationButton(carArr)
+                console.log(carArr.length)
+                }
         } else {
             //Se for diferente de menos -1 o vai adicionar no array
             if(GetCarArrPosition(carArr, carClass) !== -1){
                 carArr.splice(GetCarArrPosition(carArr, carClass), 1)
             }
+            verificationButton(carArr)
         } 
     } else {
         throw "You need set a Car Class";
+    }
+}
+
+function verificationButton(carArr){
+    const buttonCompare = document.getElementById("compare-btn")
+    if(carArr.length === 2){
+        console.log("sdja")
+        buttonCompare.setAttribute("disabled", false);
+        buttonCompare.disabled = false;
+    } else {
+        buttonCompare.setAttribute("disabled", true);
+        buttonCompare.disabled = true;
+        return
     }
 }
 
@@ -65,5 +82,40 @@ function HideCompare(){
 }
 
 function UpdateCompareTable() {
-    
+    carArr.forEach((element, index) => {
+        if(element instanceof Car){
+            let idElement = [
+                "image",
+                "nome", "alturaCacamba", 
+                "alturaVeiculo", "alturaSolo",
+                "capacidadeCarga", "motor", 
+                "potencia", "volumeCacamba", 
+                "roda", "preco"
+            ]
+
+            let idTd = [
+                "compare_image_", "compare_modelo_",
+                "compare_alturacacamba_","compare_alturaveiculo_", 
+                "compare_alturasolo_", "compare_capacidadecarga_", 
+                "compare_motor_", "compare_potencia_", 
+                "compare_volumecacamba_", "compare_roda_", 
+                "compare_preco_"
+            ]
+
+            for (let i = 0; i < idTd.length; i++){
+                let idConcat = idTd[i] + index
+                let idTdElement = document.getElementById(idConcat)
+                if (idElement[i] === "image"){
+                    idTdElement.innerHTML = `<img src="${element[idElement[i]]}" alt='imagem do carro' class='compare-image'>`
+                } else
+                    idTdElement.innerText = element[idElement[i]]
+            }
+            
+            
+        }
+    })  
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    verificationButton(carArr);
+})
